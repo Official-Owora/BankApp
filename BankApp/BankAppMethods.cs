93bd37b9a63bf5eb9dcbc5e89f99dcdbac51c58a
Official-Owora <chukwuemeka.owora@gmail.com>
2023-07-19 21:46:06 +0100

    Add project files.

---
+++ BankApp/BankAppMethods.cs
@@ -0,0 +1,40 @@
+﻿namespace BankApp
+{
+    public class BankAppMethods
+    {
+        public static decimal WithdrawalForSavingsAccount(decimal balance, decimal amountToWithdraw)
+        {
+            if (balance > 1000) 
+            {
+                balance -= amountToWithdraw;
+                return balance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Fund");
+                return balance;
+            }
+            
+        }
+        public static decimal WithdrawalForCurrentAccount(decimal balance, decimal amountToWithdraw)
+        {
+            if(balance >= 0)
+            {
+                balance -= amountToWithdraw;
+                return balance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Fund");
+                return balance;
+            }
+
+        }
+        public static decimal DepositMoneyToAnyAccount(decimal balance, decimal amount)
+        {
+            balance += amount;
+            return balance;
+        }
+    }
+    
+}
